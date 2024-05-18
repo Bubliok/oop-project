9196df85b83116ae2813f6a0fac4e6862dfcd447
bubliok <bubliok@outlook.com>
2024-05-19 01:46:03 +0300

    save/saveas xml

--- src/commands/databases/SaveAsCommand.java
+++ src/commands/databases/SaveAsCommand.java
@@ -1,62 +1,103 @@
 package commands.databases;
-
 import commands.Command;
-import handlers.DatabaseHandler;
-import models.Database;
+import handlers.CommandHandler;
+import handlers.TableFileHandler;
+import handlers.TableFileHandlerImpl;
 import models.Table;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
 
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerConfigurationException;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 
 public class SaveAsCommand implements Command {
-    private DatabaseHandler databaseHandler;
+    private CommandHandler commandHandler;
 
-    public SaveAsCommand(DatabaseHandler databaseHandler) {
-        this.databaseHandler = databaseHandler;
+    public SaveAsCommand(CommandHandler commandHandler) {
+        this.commandHandler = commandHandler;
     }
 
     @Override
     public void execute(String[] args) {
-        if (args.length < 2) {
-            System.out.println("Invalid arguments. Provide a valid file path.");
-            return;
-        }
-        String filePath = args[1];
-        File file = new File(filePath);
-        Database database = databaseHandler.getDatabase();
-        File directory = file.getParentFile();
 
-        if (file.exists()){
-            System.out.println("File already exists.");
-            return;
-        }
+            String filePath = args[1];
+            File file = new File(filePath);
+            File directory = file.getParentFile();
 
-        if (file.isDirectory()){
-            System.out.println("No file name provided.");
-            return;
-        }
-
-        if (directory == null){
-            System.out.println("Invalid file path.");
-            return;
-        }
-
-        try (BufferedWriter writer = new BufferedWriter(new FileWriter(file))) {
-            System.out.println(database);
-            for (Table table : database.getTables()) {
-                System.out.println(table);
-                writer.write(table.getTableName() + ", " + table.getTablePath());
-                writer.newLine();
+            if (args.length < 2) {
+                System.out.println("Invalid file path.");
+                return;
             }
-        } catch (IOException e) {
-            System.out.println("Error: " + e.getMessage());
-            return;
+
+            if (file.isDirectory()) {
+                System.out.println("No file name provided.");
+                return;
+            }
+
+            if (file.exists()) {
+                System.out.println("File already exists.");
+                return;
+            }
+
+            if (directory == null) {
+                System.out.println("Invalid file path.");
+                return;
+            }
+
+//            if (!directory.exists()) {
+//                directory.mkdirs();
+//            }
+
+            try {
+                DocumentBuilderFactory docFactory = DocumentBuilderFactory.newInstance();
+                DocumentBuilder docBuilder = docFactory.newDocumentBuilder();
+
+                Document doc = docBuilder.newDocument();
+                Element databaseElement = doc.createElement("database");
+                //databaseElement.setAttribute("name", commandHandler.getDatabase().getDatabaseName());
+                doc.appendChild(databaseElement);
+
+                for (Table table : commandHandler.getDatabase().getTables()) {
+                    Element tableElement = doc.createElement("table");
+
+                    String tableName = table.getTableName();
+                    Element nameElement = doc.createElement("name");
+                    nameElement.appendChild(doc.createTextNode(tableName));
+                    tableElement.appendChild(nameElement);
+
+                    Element pathElement = doc.createElement("path");
+                    pathElement.appendChild(doc.createTextNode(table.getFileHandler().getFilePath()));
+                    tableElement.appendChild(pathElement);
+                    // tableElement.appendChild(tableElement);
+                    databaseElement.appendChild(tableElement);
+
+                    TransformerFactory transformerFactory = TransformerFactory.newInstance();
+                    Transformer transformer = transformerFactory.newTransformer();
+                    DOMSource source = new DOMSource(doc);
+                    StreamResult result = new StreamResult(file);
+                    transformer.transform(source, result);
+
+                }
+                    System.out.println("Successfully saved as " + filePath);
+
+            } catch (ParserConfigurationException | TransformerException e) {
+                System.out.println("Error saving file: " + e.getMessage());
+            }
         }
-        System.out.println("Successfully saved as: " + filePath);
     }
-}
+
+
 
 
 
