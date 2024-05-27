7e9a7a8ec65aec432a0294d787b8ed05847eca3e
bubliok <bubliok@outlook.com>
2024-05-27 16:53:50 +0300

    Revert "Update Application.java"
    This reverts commit 66f1a4b3e658a4c7606cf819a33fe055b7dad6c8.

--- src/Application.java
+++ src/Application.java
@@ -5,11 +5,11 @@ import java.util.Scanner;
 public class Application {
     public static void main(String[] args) {
         CommandHandler commandHandler = new CommandHandler();
+
         Scanner scanner = new Scanner(System.in);
-        
+
         String command;
         while (!(command = scanner.nextLine()).equals("exit")) {
-
             commandHandler.handleCommand(command);
         }
         System.out.println("Exiting the program...");
